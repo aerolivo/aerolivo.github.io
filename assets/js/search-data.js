6beaa96b43ed4ff4a0471c09fab81e2149eec2f2
aerolivo <29662873+aerolivo@users.noreply.github.com>
2025-04-19 15:22:42 +0000

    Deploying to gh-pages from @ aerolivo/aerolivo.github.io@fff9d4afd9c5dfd5657091b062d103a2a05cac8d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%6E%64%65%72.%72%65%6E%6F%6C@%70%72%6F%74%6F%6E.%6D%65", "_blank");
         },
       },{
         id: 'social-inspire',
